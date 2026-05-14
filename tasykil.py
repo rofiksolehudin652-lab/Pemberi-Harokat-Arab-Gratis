d5ba80744af0e97442a593a83e86988c53b83d33
rofiksolehudin652-lab <rofiksolehudin652@gmail.com>
2026-05-14 22:09:47 +0700

    Update tasykil.py

--- tasykil.py
+++ tasykil.py
@@ -50,10 +50,8 @@ col1, col2 = st.columns(2)
 with col1:
     st.subheader("Input Teks")
     tombol = st.button("✨ Beri Harakat Sekarang")
-    text_input = st.subheader("Hasil") ("Tempelkan teks Arab di sini:", placeholder="...مرحبا كيف حالك", height=250)
-
+   text_input = st.text_area("Hasil Teks Arab:", height=300)
 with col2:
-    
     if tombol:
         if text_input.strip():
             with st.spinner("Sedang memproses..."):
